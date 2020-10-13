ad9296c0e5e385148e9a468920a573c6c437cf12
B.Cheolu <b.angelthink@gmail.com>
2020-10-14 00:12:27 +0900

    Initialize project

---
+++ generator.js
@@ -0,0 +1,125 @@
+const fs = require('fs');
+const path = require('path');
+
+const typescriptTemplate = (model, underscore, charset) => {
+	return `/**
+ * Generate with sequelize-generate-model
+ * https://github.com/cheolubak/sequelize-generate-model
+**/
+
+import {Model, DataTypes, Association, Optional} from 'sequelize';
+
+interface ${model}Attributes {
+	id: number;
+}
+
+interface ${model}CreationAttributes extends Optional<${model}Attributes, "id"> {}
+
+class ${model} extends Model<${model}Attributes, ${model}Attributes> implements ${model}Attributes {
+	public id!: number;
+	
+	public readonly ${underscore.toUpperCase() === 'Y' ? 'created_at' : 'createdAt'}!: Date;
+	public readonly ${underscore.toUpperCase() === 'Y' ? 'updated_at' : 'updatedAt'}!: Date;
+	
+	public static associations: {
+		
+	};
+}
+
+${model}.init(
+	{
+		id: {
+			type: DataTypes.INTEGER.UNSIGNED,
+			autoIncrement: true,
+			primaryKey: true,
+			allowNull: false,
+			comment: ''
+		}
+	}
+);
+
+export ${model};
+`;
+};
+const javascriptTemplate = (model, underscore, charset) => {
+	return `/**
+ * Generate with sequelize-generate-model
+ * https://github.com/cheolubak/sequelize-generate-model
+**/
+
+const {Model, DataTypes} = require('sequelize');
+
+class ${model} extends Model {
+	static init(sequelize) {
+		return super.init({
+			id: {
+				type: DataTypes.INTEGER.UNSIGNED,
+				autoIncrement: true,
+				primaryKey: true,
+				allowNull: false,
+				comment: ''
+			}
+		}, {
+			sequelize,
+			underscored: ${underscore.toUpperCase() === 'Y' ? 'true' : 'false'},
+			paranoid: true,
+			charset: '${charset}',
+			collate: '${charset}_general_ci',
+			modelName: '${model}',
+			comment: ''
+		});
+	}
+	
+	static associate(db) {
+		
+	}
+}
+`
+};
+
+const exist = (p) => {
+	try {
+		fs.accessSync(p, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK);
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
+
+const mkdirp = (p) => {
+	const pathName = path
+		.relative('.', path.normalize(p))
+		.split(path.sep)
+		.filter(p => !!p);
+	pathName.forEach((t, i) => {
+		const pathBuilder = pathName.slice(0, i + 1).join(path.sep);
+		if (!exist(pathBuilder)) {
+			fs.mkdirSync(pathBuilder);
+		}
+	})
+};
+
+const generateTemplate = (mod, model, underscore, charset, filename, p) => {
+	model = model.charAt(0).toUpperCase() + model.slice(1).toLowerCase();
+	mkdirp(p);
+	const pathToFile = path.join(p, `${filename}.${mod}`);
+	if (mod === 'ts') {
+		if (exist(pathToFile)) {
+			console.error('Already exists file');
+		} else {
+			fs.writeFileSync(pathToFile, typescriptTemplate(model, underscore, charset));
+			console.log(`Success generated ${pathToFile}`);
+		}
+	} else if (mod === 'js') {
+		if (exist(pathToFile)) {
+			console.error('Already exists file');
+		} else {
+			fs.writeFileSync(pathToFile, javascriptTemplate(model, underscore, charset));
+			console.log(`Success generated ${pathToFile}`);
+		}
+	} else {
+		console.error(`For module, enter ts or js.`);
+	}
+};
+
+module.exports = generateTemplate;
---
+++ index.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+const {program} = require('commander');
+const inquirer = require('inquirer');
+
+const generator = require('./generator');
+
+program.version('0.0.1', '-v, --version')
+	.name('sequelize-generate-model');
+
+program.command('generate')
+	.usage('--module <module> --model <model> --underscore [underscore] --charset [charset] --filename [filename] --path [path]')
+	.description('Generate sequelize model')
+	.alias('g')
+	.requiredOption('-m, --model <model>')
+	.requiredOption('-M, --module <module>')
+	.option('-u, --underscore [underscore]', 'Please enter underscore or not.(Y/N)', 'Y')
+	.option('-c, --charset [charset]', 'Please enter charset.(utf8/utf8mb4)', 'utf8')
+	.option('-f, --filename [filename]', 'Write wanna filename', 'test')
+	.option('-p, --path [path]', 'Write wanna generate path', '.')
+	.action((options) => {
+		generator(options.module, options.model, options.underscore, options.charset, options.filename, options.path);
+	});
+
+program.action((cmd, args) => {
+	if (args) {
+		console.log('Can not found command.');
+		program.help();
+	} else {
+		inquirer.prompt([
+			{
+				type: 'list',
+				name: 'module',
+				message: 'Select your module',
+				choices: ['ts', 'js']
+			},
+			{
+				type: 'input',
+				name: 'model',
+				message: 'Input your model name',
+				default: 'Test'
+			},
+			{
+				type: 'list',
+				name: 'underscore',
+				message: 'Select whether to underscore.',
+				choices: ['Y', 'N']
+			},
+			{
+				type: 'list',
+				name: 'charset',
+				message: 'Select whether to charset.',
+				choices: ['utf8', 'utf8mb4']
+			},
+			{
+				type: 'input',
+				name: 'filename',
+				message: 'Input your filename',
+				default: 'test'
+			},
+			{
+				type: 'input',
+				name: 'path',
+				message: 'Input your path',
+				default: '.'
+			},
+			{
+				type: 'confirm',
+				name: 'confirm',
+				message: 'Would you generate?'
+			}
+		]).then((answer) => {
+			if (answer.confirm) {
+				generator(answer.module, answer.model, answer.underscore, answer.charset, answer.filename, answer.path);
+			}
+		});
+	}
+}).parse(process.argv);
